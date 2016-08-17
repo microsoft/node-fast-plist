73d5a97ecce5ba1a3dfe18425e4b18bfa5cfca36
Alex Dima <alexdima@microsoft.com>
2016-08-17 10:32:02 +0200

    First cut

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# node-fast-plist
-A fast PLIST parser
+# fast-plist
+A fast PLIST parser.
+
+## Installing
+
+```sh
+npm install fast-plist
+```
+
+## Using
+
+```javascript
+var parse = require('fast-plist').parse;
+
+console.log(
+    parse(`
+        <?xml version="1.0" encoding="UTF-8"?>
+        <plist version="1.0">
+        <dict>
+            <key>name</key>
+            <string>Brogrammer</string>
+            <key>settings</key>
+            <dict>
+                <key>background</key>
+                <string>#1a1a1a</string>
+                <key>caret</key>
+                <string>#ecf0f1</string>
+                <key>foreground</key>
+                <string>#ecf0f1</string>
+                <key>invisibles</key>
+                <string>#F3FFB51A</string>
+                <key>lineHighlight</key>
+                <string>#2a2a2a</string>
+            </dict>
+        </dict>
+        </plist>`
+    )
+);
+/* Output:
+{
+        "name": "Brogrammer",
+        "settings": {
+                "background": "#1a1a1a",
+                "caret": "#ecf0f1",
+                "foreground": "#ecf0f1",
+                "invisibles": "#F3FFB51A",
+                "lineHighlight": "#2a2a2a"
+        }
+}
+*/
+```
+
+```javascript
+
+parse(`bad string`);
+
+/* Output:
+
+Error: Near offset 1: expected < ~~~ad string~~~
+*/
+```
+
+## License
+[MIT](https://github.com/Microsoft/node-fast-plist/blob/master/LICENSE.md)
---
+++ test/test.ts
@@ -0,0 +1,138 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+'use strict';
+
+import {parse} from '../src/main';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as assert from 'assert';
+import * as sax from 'sax';
+
+const FIXTURES_FOLDER_PATH = path.join(__dirname, '../../test/fixtures');
+
+describe('parse', () => {
+    let fixtures = fs.readdirSync(FIXTURES_FOLDER_PATH);
+    
+    fixtures.forEach((fixture) => {
+        it('should work for ' + fixture, () => {
+            let contents = fs.readFileSync(path.join(FIXTURES_FOLDER_PATH, fixture)).toString();
+            
+            let expected = parseWithSAX(contents).value;
+            let actual = parse(contents);
+
+            assert.deepEqual(actual, expected);
+        });
+    });
+});
+
+/**
+ * Parse a PLIST file using `sax`.
+ */
+function parseWithSAX(content: string): { value: any; errors: string[]; } {
+    
+    interface PListObject {
+        parent: PListObject;
+        value: any;
+        lastKey?: string;
+    }
+
+	let errors : string[] = [];
+	let currObject : PListObject = null;
+	let result : any = null;
+
+	let text: string = null;
+
+    let parser = sax.parser(false, { lowercase: true });
+	parser.onerror = (e:any) => {
+		errors.push(e.message);
+	};
+	parser.ontext = (s: string) => {
+		text = s;
+	};
+	parser.onopentag = (tag: sax.Tag) => {
+		switch (tag.name) {
+			case 'dict':
+				currObject = { parent: currObject, value: {} };
+				break;
+			case 'array':
+				currObject = { parent: currObject, value: [] };
+				break;
+			case 'key':
+				if (currObject) {
+					currObject.lastKey = null;
+				}
+				break;
+		}
+		text = '';
+	}
+
+	parser.onclosetag = (tagName:  string) => {
+		let value: any;
+		switch (tagName) {
+			case 'key':
+				if (!currObject || Array.isArray(currObject.value)) {
+					errors.push('key can only be used inside an open dict element');
+					return;
+				}
+				currObject.lastKey = text;
+				return;
+			case 'dict':
+			case 'array':
+				if (!currObject) {
+					errors.push(tagName + ' closing tag found, without opening tag');
+					return;
+				}
+				value = currObject.value;
+				currObject = currObject.parent;
+				break;
+			case 'string':
+			case 'data':
+				value = text;
+				break;
+			case 'date':
+				value = new Date(text);
+				break;
+			case 'integer':
+				value = parseInt(text);
+				if (isNaN(value)) {
+					errors.push(text + ' is not a integer');
+					return;
+				}
+				break;
+			case 'real':
+				value = parseFloat(text);
+				if (isNaN(value)) {
+					errors.push(text + ' is not a float');
+					return;
+				}
+				break;
+			case 'true':
+				value = true;
+				break;
+			case 'false':
+				value = false;
+				break;
+			case 'plist':
+				return;
+			default:
+				errors.push('Invalid tag name: ' + tagName);
+				return;
+
+		}
+		if (!currObject) {
+			result = value;
+		} else if (Array.isArray(currObject.value)) {
+			currObject.value.push(value);
+		} else {
+			if (currObject.lastKey) {
+				currObject.value[currObject.lastKey] = value;
+			} else {
+				errors.push('Dictionary key missing for value ' + value);
+			}
+		}
+	};
+	parser.write(content);
+	
+    return { errors: errors, value: result };
+}
